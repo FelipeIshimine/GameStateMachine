2d96c35fe0674c6e4aea1d2e205eff7367ba7aee
Ishimine <felipecalafate@hotmail.com>
2021-11-08 19:07:19 -0300

    Borradas dependencias innecesarias

--- Runtime/GameStatesManager.cs
+++ Runtime/GameStatesManager.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using GameStateMachineCore;
-using Unity.Collections;
 using UnityEditor;
 using UnityEngine;
 
